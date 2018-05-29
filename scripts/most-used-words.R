9a8ab072ceb5236da64873e67b41ce555ff0aacc
Matthew Mcvicker <23mcvicker@gmail.com>
2018-05-28 17:59:32 -0700

    Datatable with words and frequency

---
+++ scripts/most-used-words.R
@@ -0,0 +1,6 @@
+#Find the amount of times each word is used from data.
+data <- read.csv("./data/words_362689877751627777.csv")
+unique <- table(unlist(data))
+unique_words <- as.data.frame(unique)
+names(unique_words)[names(unique_words) == "Var1"] <- "Words"
+names(unique_words)[names(unique_words) == "Freq"] <- "Frequency"
