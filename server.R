1b3df8cb459ce90d7d484cd7f20c4c971cb5a197
Leo <Leo3418@users.noreply.github.com>
2018-05-30 15:18:07 -0700

    Add contents for the "Active Users" tab

--- server.R
+++ server.R
@@ -3,6 +3,9 @@ library(shiny)
 source("scripts/build-plots.R")
 
 shinyServer(function(input, output) {
+  output$most_active_users <- renderPlot({
+    build_most_active_users_plot(input$active_hour, input$active_user_num)
+  })
   output$most_used_words <- renderPlot({
     build_most_used_words_plot(input$date, input$word_num)
   })
