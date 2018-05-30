2d41e9c664b92b5a49c016b39de843d547fafcab
Leo <Leo3418@users.noreply.github.com>
2018-05-30 09:08:21 -0700

    Add sidebar contents for the Most Used Words tab

--- scripts/most-used-words.R
+++ scripts/most-used-words.R
@@ -8,9 +8,9 @@ data_all_time <- read.csv(
 # Specifies the `date` parameter in the format of `yyyy-mm-dd`. For instance, to
 # refer to January 1, 2018, use `2018-01-01`
 # The `date` parameter is optional. If it is unset, or the parameter is set to
-# `all`, then this function will process messages sent at all times.
-word_freq <- function(date = "all") {
-  if (date == "all") {
+# `All`, then this function will process messages sent at all times.
+word_freq <- function(date = "All") {
+  if (date == "All") {
     data <- data_all_time
   } else {
     data <- data_all_time[data_all_time$date == date, ]
@@ -20,4 +20,12 @@ word_freq <- function(date = "all") {
   names(unique_words)[names(unique_words) == "Var1"] <- "Words"
   names(unique_words)[names(unique_words) == "Freq"] <- "Frequency"
   unique_words
+}
+
+# Returns a vector of all dates covered by the words data set plus an item with
+# value `All`.
+# This function is useful for providing a list of possible choices in the Shiny
+# app.
+get_dates <- function() {
+  c("All", unique(data_all_time$date))
 }
--- ui.R
+++ ui.R
@@ -1,5 +1,6 @@
 # Sourcing in file and libraries
 library(shiny)
+source("scripts/most-used-words.R")
 
 shinyUI(
   navbarPage(
@@ -46,7 +47,27 @@ shinyUI(
       titlePanel("Most Used Words"),
       sidebarLayout(
         sidebarPanel(
-          
+          p(paste(
+            "In this tab, you can see statistics on words sent by the users in",
+            "the Discord channel. You can find which words are used most",
+            "frequently in the channel's messages."
+          )),
+          p(paste(
+            "You can choose a specific date below to see statistics of",
+            "messages that were sent in that date only."
+          )),
+          p(paste(
+            "The statistics shows that some words are mentioned more often in",
+            "specific days. For example, on the Christmas day, the word",
+            "'Christmas' was sent very frequently, but on other days, it was",
+            "not used commonly."
+          )),
+          selectInput(
+            "date",
+            "Date",
+            get_dates(),
+            selected = "All"
+          )
         ),
         mainPanel(
           
